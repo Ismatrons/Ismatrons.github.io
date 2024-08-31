84bf6bc7c8965a4b1db0442a1602975f63fd05e0
Ismatrons <106910636+Ismatrons@users.noreply.github.com>
2024-08-31 12:43:59 +0200

    Add files via upload

---
+++ scripttimer.js
@@ -0,0 +1,22 @@
+const countdown = () => {
+    const targetDate = new Date("September 19, 2024 00:00:00").getTime();
+    const now = new Date().getTime();
+    const gap = targetDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = Math.floor(gap / day);
+    const hours = Math.floor((gap % day) / hour);
+    const minutes = Math.floor((gap % hour) / minute);
+    const seconds = Math.floor((gap % minute) / second);
+
+    document.getElementById("days").innerText = String(days).padStart(2, '0');
+    document.getElementById("hours").innerText = String(hours).padStart(2, '0');
+    document.getElementById("minutes").innerText = String(minutes).padStart(2, '0');
+    document.getElementById("seconds").innerText = String(seconds).padStart(2, '0');
+};
+
+setInterval(countdown, 1000);
